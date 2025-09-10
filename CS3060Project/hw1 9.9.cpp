b1a682347be298778f6bf1ef9aa7e6e393f398d1
NovaTrace <75408164+NovaTrace@users.noreply.github.com>
2025-09-10 09:39:05 -0600

    Split into 2 Projects

--- CS3060Project/hw1 9.9.cpp
+++ CS3060Project/hw1 9.9.cpp
@@ -1,6 +1,8 @@
 //Quinn Hackenberg
 //CS 3060
-//CPP hw1
+//cpp hw1
+
+void displayArray(int* arr, int size);
 
 #include <iostream>
 using namespace std;
@@ -22,6 +24,10 @@ int main() {
         cin >> *(ptrArray + i);
     }
 
+    cout << "Array before sorting: ";
+    displayArray(ptrArray, size);
+    
+
     // Bubble sort
     for (int i = 0; i < size - 1; i++) {
 
@@ -35,17 +41,29 @@ int main() {
             }
         }
     }
+    cout << "Array after sorting: ";
+    displayArray(ptrArray, size);
 
     if (size % 2 == 0) {
         
-        cout << double(*(ptrArray + (size / 2)) + *(ptrArray - 1 + (size / 2))) / 2;
+        cout << "Median: " << double(*(ptrArray + (size / 2)) + *(ptrArray - 1 + (size / 2))) / 2;
     }
 
     else {
 
-        cout << *(ptrArray + (size / 2));
+        cout << "Median: " << * (ptrArray + (size / 2));
     }
 
     delete[] ptrArray;
     return 0;
+}
+
+
+void displayArray(int* arr, int size) {
+
+    for (int i = 0; i < size; i++) {
+
+        cout << *(arr + i);
+    }
+    cout << "\n";
 }
