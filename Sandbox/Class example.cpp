f3e80e5e3c55af6c4531c98e111815410997fb44
NovaTrace <75408164+NovaTrace@users.noreply.github.com>
2025-09-22 09:20:54 -0600

    classes and constructors

---
+++ Sandbox/Class example.cpp
@@ -0,0 +1,80 @@
+﻿//メモ
+// Damn Monkeys - classes
+//
+//#include <bits/stdc++.h>
+//using namespace std;
+//class Monkey {
+//	public:
+//	string monkeyname;
+//	void printname()
+//	{
+//		cout << "Monkey name is: " << monkeyname;
+//	}
+//};
+//
+//int main()
+//{
+//	Monkey obj1;
+//	obj1.monkeyname = "vervet";
+//	obj1.printname();
+//	return 0;
+//}
+
+//using namespace std;
+//
+//// Abstract class
+//class Person {
+//public:
+//	string name;
+//	virtual void role() = 0; // pure virtual function - makes person abstract
+//};
+//
+//// Concrete class
+//class Student : public Person {
+//public:
+//	void role() override {
+//		cout << name << " is a Student." << endl;
+//	}
+//};
+//
+//int main() {
+//	Student s;
+//	s.name = "Alice";
+//	s.role();
+//}
+
+
+//メモ
+//class Rectangle
+//{
+//private:
+//	double width;
+//	double height;
+//public:
+//	void setWidth(double);
+//	void setHeight(double);
+//	double getWidth() const;
+//	double getLength() const;
+//	double getArea() const;
+//};
+
+
+//メモ - constructors and destructors
+
+#include <iostream>
+class C1 {
+	int i = 0;
+public:
+	void setvalue(int value) { i = value; }
+	int getvalue() { return i; }
+};
+
+int main() {
+	int i = 47;
+	C1 o1;
+
+	o1.setvalue(i);
+	printf("value is %d\n", o1.getvalue());
+	return 0;
+}
+
--- Sandbox/sandbox.cpp
+++ Sandbox/sandbox.cpp
@@ -1,120 +1,4 @@
-﻿//////#include <iostream>
-//////#include<iomanip>
-//////using namespace std;
-//////
-////////---// main 1 -> temperature conversion
-//////int main1()
-//////{
-//////	double celsius,
-//////		fahrenheit;
-//////
-//////	cout<<"enter the temperature in celsius:";
-//////	cin >> celsius;
-//////
-//////	fahrenheit = ((9.0 / 5.0) * celsius) + 32;
-//////
-//////	
-//////	cout << fixed << showpoint << setprecision(1) << fahrenheit << "degrees fahrenheit.\n";
-//////	//メモ << 'not scientific notation' << show decimal points* << precision after decimal <<
-//////
-//////	return 0;
-//////}
-//////
-////////----// main 2 -> leap year code
-//////
-//////bool isleapyear(int year); // prototype for subalgorithm
-//////
-//////int main2()
-//////{
-//////	int year;
-//////	cout << "enter the year:"<<endl;
-//////	cin >> year;
-//////
-//////
-//////	if (isleapyear(year)) {
-//////
-//////		cout<<year<<" is a leap year."<<endl;
-//////	}
-//////
-//////	else {
-//////
-//////		cout << year << " is not a leap year." << endl;
-//////	}
-//////
-//////	return 0;
-//////}
-//////
-//////bool isleapyear(int year)
-//////{
-//////
-//////	if (year % 4 != 0)
-//////		return false;
-//////
-//////	if (year % 100 != 0)
-//////		return true;
-//////
-//////	return true; // is yeawr not a multiple of 400
-//////}
-//////
-////////---// main 3 -> 3 number sorting code (takes 3 number inputs, outputs in ascending order seaparated by one space)
-//////
-//////int main3()
-//////{
-//////	
-//////	double num1, num2, num3;// declare vars
-//////	double temp;
-//////	cout << "enter three numbers: ";
-//////	cin >> num1 >> num2 >> num3;
-//////	cout << endl;
-//////
-//////	if (num1 > num2)
-//////	{
-//////		temp = num1;
-//////		num1 = num2;
-//////		num2 = temp;
-//////	}
-//////
-//////	// if num1 > num2  we'll need a temp to switcharoo
-//////	****
-//////
-//////		// ask for 3 number input
-//////		****
-//////
-//////		// compare num1 to num2 
-//////		// tell output
-//////
-//////		return 0;
-//////}
-//////
-////////---// main 4 -> telephone nubers (converts words to telephone numbers)
-//////
-//////#include <string>
-//////
-//////int main4()
-//////{
-//////	char letter;
-//////	int noofletters;
-//////	char response;
-//////
-//////	cout << "enter y/y to convert a telephone number " << "form letters to digits.\n" << "enter any other letter to terminate the program" << endl;
-//////
-//////	cin >> response;
-//////
-//////	while (response == 'y' || response == 'y')
-//////	{
-//////
-//////		cout << "enter a telepone number using letters: ";
-//////		cin >> letter;
-//////		cout << endl;
-//////
-//////		while (noofletters < 7)
-//////		{
-//////			noofletters++;
-//////
-//////			switch (letter)
-//////			{
-//////
-////#include <iostream>
+﻿////#include <iostream>
 ////#include<iomanip>
 ////using namespace std;
 ////
@@ -124,12 +8,12 @@
 ////	double celsius,
 ////		fahrenheit;
 ////
-////	cout << "enter the temperature in celsius:";
+////	cout<<"enter the temperature in celsius:";
 ////	cin >> celsius;
 ////
 ////	fahrenheit = ((9.0 / 5.0) * celsius) + 32;
 ////
-////
+////	
 ////	cout << fixed << showpoint << setprecision(1) << fahrenheit << "degrees fahrenheit.\n";
 ////	//メモ << 'not scientific notation' << show decimal points* << precision after decimal <<
 ////
@@ -143,13 +27,13 @@
 ////int main2()
 ////{
 ////	int year;
-////	cout << "enter the year:" << endl;
+////	cout << "enter the year:"<<endl;
 ////	cin >> year;
 ////
 ////
 ////	if (isleapyear(year)) {
 ////
-////		cout << year << " is a leap year." << endl;
+////		cout<<year<<" is a leap year."<<endl;
 ////	}
 ////
 ////	else {
@@ -176,7 +60,7 @@
 ////
 ////int main3()
 ////{
-////
+////	
 ////	double num1, num2, num3;// declare vars
 ////	double temp;
 ////	cout << "enter three numbers: ";
@@ -191,15 +75,15 @@
 ////	}
 ////
 ////	// if num1 > num2  we'll need a temp to switcharoo
-////	
+////	****
 ////
 ////		// ask for 3 number input
-////		
+////		****
 ////
 ////		// compare num1 to num2 
 ////		// tell output
 ////
-////	return 0;
+////		return 0;
 ////}
 ////
 //////---// main 4 -> telephone nubers (converts words to telephone numbers)
@@ -230,37 +114,124 @@
 ////			switch (letter)
 ////			{
 ////
-////			}
-////		}
-////	}
-////}
-////
-///////// quiz
-////
-////
-//////q1
-////
-////// int -> the variable that is incrimented used to count the loops --- initialization expression
-////// j<10 -> the algebra declaring max loop counts, ends the loop when condition is not met --- test condition
-////// j++ -> increments the int by 1 each loop, loop counter --- increment expression
-////
-//////q2
-////
-////// int qempty(){return(f==r?1:0)}
-////
-////
-////
-///////// quiz
-////
-////
-//////q1
-////
-////// int -> the variable that is incrimented used to count the loops --- initialization expression
-////// j<10 -> the algebra declaring max loop counts, ends the loop when condition is not met --- test condition
-////// j++ -> increments the int by 1 each loop, loop counter --- increment expression
-////
-//////q2
-////
-////// int qempty(){return(f==r?1:0)}
-////
-////
+//#include <iostream>
+//#include<iomanip>
+//using namespace std;
+//
+////---// main 1 -> temperature conversion
+//int main1()
+//{
+//	double celsius,
+//		fahrenheit;
+//
+//	cout << "enter the temperature in celsius:";
+//	cin >> celsius;
+//
+//	fahrenheit = ((9.0 / 5.0) * celsius) + 32;
+//
+//
+//	cout << fixed << showpoint << setprecision(1) << fahrenheit << "degrees fahrenheit.\n";
+//	//メモ << 'not scientific notation' << show decimal points* << precision after decimal <<
+//
+//	return 0;
+//}
+//
+////----// main 2 -> leap year code
+//
+//bool isleapyear(int year); // prototype for subalgorithm
+//
+//int main2()
+//{
+//	int year;
+//	cout << "enter the year:" << endl;
+//	cin >> year;
+//
+//
+//	if (isleapyear(year)) {
+//
+//		cout << year << " is a leap year." << endl;
+//	}
+//
+//	else {
+//
+//		cout << year << " is not a leap year." << endl;
+//	}
+//
+//	return 0;
+//}
+//
+//bool isleapyear(int year)
+//{
+//
+//	if (year % 4 != 0)
+//		return false;
+//
+//	if (year % 100 != 0)
+//		return true;
+//
+//	return true; // is yeawr not a multiple of 400
+//}
+//
+////---// main 3 -> 3 number sorting code (takes 3 number inputs, outputs in ascending order seaparated by one space)
+//
+//int main3()
+//{
+//
+//	double num1, num2, num3;// declare vars
+//	double temp;
+//	cout << "enter three numbers: ";
+//	cin >> num1 >> num2 >> num3;
+//	cout << endl;
+//
+//	if (num1 > num2)
+//	{
+//		temp = num1;
+//		num1 = num2;
+//		num2 = temp;
+//	}
+//
+//	// if num1 > num2  we'll need a temp to switcharoo
+//	
+//
+//		// ask for 3 number input
+//		
+//
+//		// compare num1 to num2 
+//		// tell output
+//
+//	return 0;
+//}
+//
+////---// main 4 -> telephone nubers (converts words to telephone numbers)
+//
+//#include <string>
+//
+//int main4()
+//{
+//	char letter;
+//	int noofletters;
+//	char response;
+//
+//	cout << "enter y/y to convert a telephone number " << "form letters to digits.\n" << "enter any other letter to terminate the program" << endl;
+//
+//	cin >> response;
+//
+//	while (response == 'y' || response == 'y')
+//	{
+//
+//		cout << "enter a telepone number using letters: ";
+//		cin >> letter;
+//		cout << endl;
+//
+//		while (noofletters < 7)
+//		{
+//			noofletters++;
+//
+//			switch (letter)
+//			{
+//
+//			}
+//		}
+//	}
+//}
+//
